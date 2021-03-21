4a5fc96e355f750bf9b4d9a65e68784b363be9fc
bharattank <bharattank903@gmail.com>
2021-03-21 12:05:45 +0530

    initial commit

--- mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "nilkanthsolution@gmail.com";
-$subject = $_POST['subject'];
-$txt = $_POST['message'];
-$headers = "From: $_POST['email']" . "\r\n" .
-"CC: $_POST['email']";
-
-mail($to,$subject,$txt,$headers);
-?>
